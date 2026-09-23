2126c9b43a30808d358fe5937c0d731647e647cf
fantom275 <illaorlik635@gmail.com>
2026-09-23 14:15:06 +0300

    main5.py

--- main5.py
+++ main5.py
@@ -1,11 +1,12 @@
+# This program checks if the given numbers are palindromes or not.
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 
-if str(n1) == str(n1)[::-1] and str(n2) == str(n2)[::-1]:
-    print("The both numbers are palindromes")
-elif str(n1) == str(n1)[::-1]:
-    print("The first number is a palindrome")
-elif str(n2) == str(n2)[::-1]:
-    print("The second number is a palindrome")
+if str(n1) == str(n1)[::-1]:
+    print(True)
 else:
-    print("Neither number is a palindrome")
+    print(False)
+if str(n2) == str(n2)[::-1]:
+    print(True)
+else:
+    print(False)
